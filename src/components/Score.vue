bbef2b9f64b63e16b0e7b49bbaf0439b192e4c24
Maks Kolman <kolman.maks@gmail.com>
2019-08-25 23:57:32 +0100

    Have persistent storage of scores (and new icons!)

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,15 +1,48 @@
 <template>
   <div>
     <h1>SCORE</h1>
-    <span class="btn scr">
-      <span v-on:click="score -= 5">–5</span
-      ><span v-on:click="score -= 1" class="border">–1</span
-      ><input type="number" v-bind:value="score" /><span
-        v-on:click="score += 1"
+    <span class="btn scr" v-bind:key="index" v-for="(score, index) in scores">
+      <span
+        v-if="score.alt == 0"
+        v-on:click="score.value -= 5"
+        >–5</span
+      ><span
+        v-if="score.alt == 0"
+        v-on:click="score.value -= 1"
+        class="border"
+        >–1</span
+      ><span
+        v-if="score.alt == 1"
+        v-on:click="score.alt = 0"
+        ><font-awesome-icon icon="backward" /></span
+      ><span
+        v-if="score.alt == 1"
+        v-on:click="scores.push({value: score.value, alt: 0})"
+        class="border"
+        ><font-awesome-icon v-bind:icon="{ prefix: 'far', iconName: 'clone' }" /></span
+      ><input
+        type="number"
+        v-bind:value="score.value"
+        v-on:focus="score.alt = 1" /><span
+        v-if="score.alt == 0"
+        v-on:click="score.value += 1"
         class="border"
         >+1</span
-      ><span v-on:click="score += 5">+5</span>
+      ><span
+        v-if="score.alt == 0"
+        v-on:click="score.value += 5"
+        >+5</span
+      ><span
+        v-if="score.alt == 1"
+        v-on:click="scores.splice(index, 1)"
+        class="border"
+        ><font-awesome-icon v-bind:icon="{ prefix: 'far', iconName: 'trash-alt' }" /></span
+      ><span
+        v-if="score.alt == 1"
+        v-on:click="score.value = 0"
+        ><font-awesome-icon  icon="undo"/></span>
     </span>
+    <span class="btn" v-on:click="scores.push({value: 0, alt: 0})"><span> + </span></span>
   </div>
 </template>
 
@@ -18,8 +51,32 @@ export default {
   name: "Score",
   data() {
     return {
-      score: 0
+      scores: []
     };
+  },
+  watch: {
+    scores: {
+       handler(){
+        localStorage.setItem('scores', JSON.stringify(this.scores));
+       },
+       deep: true
+    }
+  },
+  mounted() {
+    this.scores = this.loadScores();
+  },
+  methods: {
+    loadScores: function() {
+      let scores = localStorage.getItem('scores');
+      if (scores) {
+        try {
+          return JSON.parse(scores);
+        } catch(e) {
+          localStorage.removeItem('scores');
+        }
+      }
+      return [{value: 0, alt: 0}];
+    }
   }
 };
 </script>
