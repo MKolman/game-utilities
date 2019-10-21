4de4057988cfec79eb58060d17d0423f471d59da
Maks Kolman <kolman.maks@gmail.com>
2019-10-21 21:09:54 +0100

    Allow multi level word storage and dropdown

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,13 +1,23 @@
 <template>
   <md-field class="md-layout-item md-size-45 md-small-size-95">
     <md-select class="md-elevation-1" v-model="localValue" multiple>
-      <md-option
-        :value="itemName"
-        :key="itemName"
-        v-for="itemName in Object.keys(options)"
-      >
-        {{ formatListName(itemName) }} ({{ options[itemName].length }})
+      <md-option :value="item.key" :key="item.key" v-for="item in items">
+        {{ item.name }} ({{ item.length }})
       </md-option>
+
+      <md-optgroup
+        :label="group.name + ' (' + group.length + ')'"
+        :key="group.key"
+        v-for="group in itemGroups"
+      >
+        <md-option
+          :value="item.key"
+          :key="item.key"
+          v-for="item in group.items"
+        >
+          {{ item.name }} ({{ item.length }})
+        </md-option>
+      </md-optgroup>
     </md-select>
   </md-field>
 </template>
@@ -17,6 +27,28 @@ export default {
   props: {
     options: Object,
     value: Array
+  },
+  computed: {
+    items() {
+      return this.getItems(this.options);
+    },
+    itemGroups() {
+      let result = [];
+      for (let key in this.options) {
+        if (!key.endsWith(".txt")) {
+          let items = this.getItems(this.options[key]);
+          result.push({
+            key: key,
+            items: items,
+            name: this.formatListName(key),
+            length: items.reduce((t, x) => {
+              return t + x.length;
+            }, 0)
+          });
+        }
+      }
+      return result;
+    }
   },
   data() {
     return {
@@ -32,8 +64,25 @@ export default {
     }
   },
   methods: {
+    getItems(srcData) {
+      let result = [];
+      for (let key in srcData) {
+        if (key.endsWith(".txt")) {
+          result.push({
+            key: key,
+            name: this.formatListName(key),
+            length: srcData[key].length
+          });
+        }
+      }
+      result.sort((a, b) => {
+        return a.key < b.key ? -1 : 1;
+      });
+      return result;
+    },
     formatListName(listName) {
       listName = listName
+        .replace(".txt", "")
         .replace("_", " ")
         .replace(/\b\w/g, l => l.toUpperCase());
       return listName;
--- src/data/wordlist/update_wordlist.py
+++ src/data/wordlist/update_wordlist.py
@@ -1,17 +1,32 @@
 import os
 import json
 
+def update_dict(data, key_list, value):
+    if len(key_list) == 0:
+        return
+    key = key_list[0]
+    if len(key_list) == 1:
+        data[key] = value
+    else:
+        if key not in data:
+            data[key] = {}
+        update_dict(data[key], key_list[1:], value)
+
 def build_result_json():
     result = {}
     for path, subfolders, files in os.walk('.'):
         if path == '.':
             continue
-        key = path[2:] if path.startswith("./") else path
-        result[key] = {}
+        key = path.split('/')
         for filename in filter(lambda x: x.endswith('.txt'), files):
+            # print(path, filename, list(result.get('.', {}).get('si', {})))
             rows = open(os.path.join(path, filename)).readlines()
-            result[key][filename[:-4]] = [row.strip() for row in rows]
-    return result
+            update_dict(
+                result, key+[filename], [row.strip() for row in rows]
+            )
+    print(list(result['.']))
+    print(list(result['.']['en']))
+    return result['.']
 
 def create_json_file():
     with open('data.json', 'w') as jsonfile:
@@ -19,3 +34,4 @@ def create_json_file():
 
 if __name__ == "__main__":
     create_json_file()
+    # print(make_dict(['a', 'b'], 1))
