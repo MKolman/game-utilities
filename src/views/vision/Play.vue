18d678a141c986bf99f1d3aa0e6c234180a5cb91
Maks Kolman <kolman.maks@gmail.com>
2019-12-01 13:53:22 +0000

    Lint Play.vue

--- src/views/vision/Play.vue
+++ src/views/vision/Play.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="vision-play">
-    <div class=drops>
+    <div class="drops">
       <span class="neutral"></span>
       <span class="bad"></span>
       <span class="good"></span>
     </div>
     <span class="word">
       <span class="flip">wording1</span>
-      <br/>
+      <br />
       <span>wording2</span>
     </span>
     <span class="timer">
@@ -23,59 +23,58 @@ export default {
 </script>
 
 <style lang="scss">
-  #vision-play {
-    position: relative;
-    display: flex;
-    flex: 1;
-    flex-flow: column;
-    width: 100%;
-    height: 100%;
-    justify-content: space-evenly;
-    align-items: center;
-    writing-mode: vertical-rl;
-    .drops {
+#vision-play {
+  position: relative;
+  display: flex;
+  flex: 1;
+  flex-flow: column;
+  width: 100%;
+  height: 100%;
+  justify-content: space-evenly;
+  align-items: center;
+  writing-mode: vertical-rl;
+  .drops {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    span {
       position: absolute;
-      left: 0;
-      right: 0;
-      top: 0;
+      box-shadow: var(--box-shadow-color) 0px 0px 100px 20px;
+    }
+    .good {
       bottom: 0;
-      span {
-        position: absolute;
-        box-shadow: var(--box-shadow-color) 0px 0px 100px 20px;
-      }
-      .good {
-        bottom: 0;
-        left: 25%;
-        width: 50%;
-        --box-shadow-color: green;
-      }
-      .bad {
-        top: 0;
-        left: 25%;
-        width: 50%;
-        --box-shadow-color: red;
-      }
-      .neutral {
-        top: 25%;
-        right: 0;
-        height: 50%;
-        --box-shadow-color: blue
-      }
-
+      left: 25%;
+      width: 50%;
+      --box-shadow-color: green;
     }
-    .word {
-      font-size: 100px;
-      line-height: 100px;
-      span {
-        display: inline-block;
-      }
-      .flip {
-        transform: rotate(180deg);
-      }
+    .bad {
+      top: 0;
+      left: 25%;
+      width: 50%;
+      --box-shadow-color: red;
     }
-    .timer {
-      font-size: 40px;
-      line-height: 40px;
+    .neutral {
+      top: 25%;
+      right: 0;
+      height: 50%;
+      --box-shadow-color: blue;
     }
   }
+  .word {
+    font-size: 100px;
+    line-height: 100px;
+    span {
+      display: inline-block;
+    }
+    .flip {
+      transform: rotate(180deg);
+    }
+  }
+  .timer {
+    font-size: 40px;
+    line-height: 40px;
+  }
+}
 </style>
